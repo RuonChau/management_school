3ff1da1d7bc546919419e609031f926cabc13851
ChauRuon <chauruon.lci@gmai.com>
2026-03-15 11:41:32 +0700

    update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,15 +7,15 @@ import { Navbar } from "@/components/Navbar";
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
   return (
-    <div className="h-screen flex ">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 ">
+    <div className="flex ">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 h-screen sticky ">
         <Link href={'/'} className="flex items-center justify-center lg:justify-start gap-2">
           <Image src={logo} alt="logo" width={wh_logo} height={wh_logo}/>
           <span className="hidden lg:block">Lavin</span>
         </Link>
         <Menu/>
       </div>
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-auto">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] dark:bg-slate-900 overflow-auto">
         <Navbar/>
         {children}
       </div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 
 const Menu = () => {
   return (
-    <div className="text-sm">
+    <div className="text-sm ">
       {menuItem.map(i =>(
-        <div className="flex flex-col gap-1" key={i.title}>
+        <div className="flex flex-col gap-1 " key={i.title}>
           <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
           {i.items.map(item =>(
             <Link href={item.href} key={item.lable}
-              className="text-gray-500 py-2 flex items-center justify-center lg:justify-start gap-4 " >
+              className=" text-gray-500 py-2 flex items-center justify-center lg:justify-start gap-4 " >
               <Image src={item.icon} alt={item.lable} width={wh_menu} height={wh_menu}/>
               <span className="hidden lg:block">{item.lable}</span>
             </Link>
